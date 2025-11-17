b9696098994d26b253bef3eb7e0202f572cc062b
Shivansh Mishra <shivanshm665@gmail.com>
2025-11-17 23:55:38 +0530

    Random Password Generator

---
+++ Random Password Generator/script.js
@@ -0,0 +1,36 @@
+function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
+
+    const lowercaseChars = "abcdefghijklmnopqrstuvxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+=";
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    if(length < 0){
+        return `(password length must be atleast 1)`;
+    }
+
+    for(let i = 0; i<length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+
+    return password;
+}
+
+const passwordLength =  12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+
+console.log(`Generate Password: ${password}`);
